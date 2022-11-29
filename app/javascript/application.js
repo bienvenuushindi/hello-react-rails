495790a8f76f4b316aa5e5892f9b60aea5df78bd
usbbush <jeanbienvenusb@gmail.com>
2022-11-29 13:05:04 +0200

    Render api data

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,23 +1,30 @@
 // Entry point for the build script in your package.json
 // import "@hotwired/turbo-rails"
 // import "./controllers"
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import Greeting from "./greeting";
+import Greeting from "./components/greeting";
+import {Provider, useDispatch, useSelector} from "react-redux";
+import {fetchGreetings} from "./redux/actions";
+import store from "./redux/store";
 
 function App() {
-    const message = 'Hello, Holla, Salut'
+    const dispatch = useDispatch();
+    const greetings = useSelector((state) => state.greetings);
+    useEffect(() => {
+        if (greetings.length === 0) dispatch(fetchGreetings());
+    }, [])
     return (
         <>
             <h1>Get a random greeting</h1>
-            <Greeting message={message}/>
+            <Greeting message={greetings[Math.floor(Math.random() * greetings.length)]}/>
         </>
-
-
     );
 }
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root'),
 );
--- app/javascript/components/greeting.js
+++ app/javascript/components/greeting.js
@@ -1,12 +1,17 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const Greeting = (props) => {
     const {message} = props
     return (
         <div>
-            {message}
+            {message || 'Loading Greeting'}
         </div>
     )
 }
+
+Greeting.propTypes = {
+    message: PropTypes.string.isRequired,
+};
 
 export default Greeting
